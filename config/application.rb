be190563af295d34510e2b859f1b98e82f389cc0
aizwellenstan <aizwellenstan@gmail.com>
2020-02-06 11:41:24 +0900

    fix cors

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module FileUploader
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.hosts << "*"
+    config.hosts << "product.nadi3docms.com"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
