8d153e3db4f82d7fdab16de1536ed034bc7145e3
aizwellenstan <aizwellenstan@gmail.com>
2020-02-06 11:37:21 +0900

    nothing

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,7 @@ Bundler.require(*Rails.groups)
 module FileUploader
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-    config.hosts << ".com"
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
