9b12f05e901091dfc1b53fb99c948ef0b905d822
aizwellenstan <aizwellenstan@gmail.com>
2020-02-06 11:39:53 +0900

    fix cors

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module FileUploader
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.hosts << "*"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
