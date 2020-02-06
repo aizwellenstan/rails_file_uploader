b73390e7a90499e397290b4ff4bd7939aae4ef56
aizwellenstan <aizwellenstan@gmail.com>
2020-02-06 11:11:58 +0900

    add cors

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module FileUploader
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
     # Permit cross origin
     config.middleware.insert_before 0, Rack::Cors do
       allow do
